5791dc7bd7182c2d73481f7d53e9e3474ce789a3
NMHoangDev <ngminhhoang0934@gmail.com>
2025-10-19 01:40:45 +0700

    feature: add component movie section

---
+++ src/data/sampleMoviesData.js
@@ -0,0 +1,72 @@
+import imgPosterFimlKhongTamNhan from  '../assets/image/img-poster-film-khong-tam-nhan.png'
+import imgPosterFilmAdulthood from '../assets/image/img-poster-film-adulthood.png';
+import imgPoserFilmThanDenOiUocDi from '../assets/image/img-poster-fim-than-den.png';
+import imgPosterFilmMuaHeDiaNguc from '../assets/image/img-poster-film-mua-he-dia-nguc.png';
+import imgPosterFilmTheConjuring from '../assets/image/img-poster-film-conjuring.png';
+
+export const sampleMoviesData = [
+  {
+    id: 1,
+    title: "Quái Nhân Báo Thù",
+    subtitle: "The Toxic Avenger",
+    image: imgPosterFilmMuaHeDiaNguc,
+    ageRating: "PD. 18",
+    category: "TM. 16"
+  },
+  {
+    id: 2,
+    title: "Quái Nhân Báo Thù   ",
+    subtitle: "The Toxic Avenger",
+    image: imgPosterFimlKhongTamNhan,
+    ageRating: "PD. 18",
+    category: "TM. 16"
+  },
+  {
+    id: 3,
+    title: "Quái Nhân Báo Thù",
+    subtitle: "The Toxic Avenger",
+    image: imgPosterFilmTheConjuring,
+    ageRating: "PD. 18",
+    category: "TM. 16"
+  },
+  {
+    id: 4,
+    title: "Quái Nhân Báo Thù",
+    subtitle: "Adulthood",
+    image: imgPosterFilmAdulthood,
+    ageRating: "PD. 16",
+    category: "TM. 16"
+  },
+  {
+    id: 5,
+    title: "Quái Nhân Báo Thù",
+    subtitle: "The Toxic Avenger",
+    image: imgPosterFilmMuaHeDiaNguc,
+    ageRating: "PD. 18",
+    category: "TM. 16"
+  },
+  {
+    id: 6,
+    title: "Quái Nhân Báo Thù",
+    subtitle: "The Toxic Avenger",
+    image: imgPoserFilmThanDenOiUocDi,
+    ageRating: "PD. 16",
+    category: "TM. 16"
+  },
+  {
+    id: 7,
+    title: "Quái Nhân Báo Thù",
+    subtitle: "The Toxic Avenger",
+    image: imgPosterFilmAdulthood,
+    ageRating: "PD. 18",
+    category: "TM. 16"
+  },
+  {
+    id: 8,
+    title: "Quái Nhân Báo Thù",
+    subtitle: "The Toxic Avenger",
+    image: imgPosterFimlKhongTamNhan,
+    ageRating: "PD. 18",
+    category: "TM. 16"
+  }
+];
